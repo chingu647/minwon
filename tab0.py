0f365502a4dffdcbd4348b1db0cd42c49bf47ac5
chingu647 <chingu647@gmail.com>
2024-01-17 11:29:18 +0900

    all...

--- tab0.py
+++ tab0.py
@@ -3,35 +3,35 @@ import streamlit as st
 
 def run_tab(): 
     # ----------------------------------------------------------------------- layout 
-    head0, head1, head2 = st.columns( [0.2, 0.6, 0.2] )
-    headt0,headt1,headt2= st.columns( [0.01, 0.98, 0.01] )
+    t0_head0, t0_head1, t0_head2 = st.columns( [0.2, 0.6, 0.2] )
+    t0_headt0,t0_headt1,t0_headt2= st.columns( [0.01, 0.98, 0.01] )
 
-    body0, body1, body2 = st.columns( [0.2, 0.6, 0.2] )
-    bodyt0,bodyt1,bodyt2= st.columns( [0.01, 0.98, 0.01] )
+    t0_body0, t0_body1, t0_body2 = st.columns( [0.2, 0.6, 0.2] )
+    t0_bodyt0,t0_bodyt1,t0_bodyt2= st.columns( [0.01, 0.98, 0.01] )
 
-    tail0, tail1, tail2 = st.columns( [0.2, 0.6, 0.2] ) 
-
-    # -----------------------------------------------------------------------  
-    head0.markdown("###### 오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    head0.markdown("오늘의 이슈") 
-    # -----------------------------------------------------------------------  
-    head1.markdown("###### 민원건수 현황") 
+    t0_tail0, t0_tail1, t0_tail2 = st.columns( [0.2, 0.6, 0.2] ) 
 
     # -----------------------------------------------------------------------  
-    head2.markdown("###### 지사.노선별 현황") 
+    t0_head0.markdown("###### 오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    t0_head0.markdown("오늘의 이슈") 
+    # -----------------------------------------------------------------------  
+    t0_head1.markdown("###### 민원건수 현황") 
+
+    # -----------------------------------------------------------------------  
+    t0_head2.markdown("###### 지사.노선별 현황") 
     
     # -----------------------------------------------------------------------  
-    headt1.markdown("""---""")     
+    t0_headt1.markdown("""---""")     
     # -----------------------------------------------------------------------  
-    body0.markdown("###### 민원 키워드 클라우드") 
+    t0_body0.markdown("###### 민원 키워드 클라우드") 
 
     # -----------------------------------------------------------------------  
-    body1.markdown("###### 공지사항") 
+    t0_body1.markdown("###### 공지사항") 
 
