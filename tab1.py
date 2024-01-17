f3128fc06cb70f93b739382b60e06d673aa774c1
chingu647 <chingu647@gmail.com>
2024-01-17 18:57:32 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -86,7 +86,7 @@ def run_tab():
     folium.GeoJson(t1_gpf_line,
                    style_function=lambda feature: {
                     #    'fillColor': feature['properties']['color'],
-                       'color': '#ff00ff',
+                       'color': '#c0c0c0',
                        'weight': 1,
                        'dashArray': '5, 5',
                        'fillOpacity': 0.7, 
