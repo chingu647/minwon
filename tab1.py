a2cd18bab4c0b10382cdbb160b7e9fb06e24b2ba
chingu647 <chingu647@gmail.com>
2024-01-17 14:19:09 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -69,12 +69,12 @@ def run_tab():
 
     # map 
     t1_gpf = gpd.read_file("data/ex_point_KWANGJU.geojson")
-    t1_gpf = t1_gpf[ ['노선번호','geometry'] ]
-    t1_tail1.write(t1_gpf) 
+    t1_gpf = t1_gpf[ ['노선번호','X_좌표값', 'Y좌표값'] ]
+    t1_tail1.dataframe(t1_gpf) 
     # t1_gpf = {'latitude':[37.7749,34.0522,40.7128],
     #                'longitude':[126.87954220,126.87554220,126.87964220]}
     t1_gpf_df = gpd.GeoDataFrame(t1_gpf) 
-    t1_tail1.write(t1_gpf_df)   
+    t1_tail1.dataframe(t1_gpf_df)   
 
     # base_position = [35.18668601, 126.87954220] 
 
