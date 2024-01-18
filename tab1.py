6ae0926de3222982029f61e1cb4b967aba8cc555
chingu647 <chingu647@gmail.com>
2024-01-18 22:23:43 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -79,7 +79,7 @@ def run_tab():
     data_y = t1_body1_df_gby_kind['건수']
 
     fig1, ax1 = plt.subplots(figsize=(10,4)) 
-    ax1.axis('off') 
+    ax1.axis('on') 
     ax1.bar(data_x, data_y)
     t1_body2.pyplot(fig1) 
     
