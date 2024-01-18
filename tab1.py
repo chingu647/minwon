fa71010e1d3a174198d796a010255ff5badfa06f
chingu647 <chingu647@gmail.com>
2024-01-18 23:26:03 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -67,12 +67,11 @@ def run_tab():
     t1_body2.pyplot(fig) 
 
     # -----------------------------------------------------------------------  
-    font_path_ = "data/NanumGothic.ttf" 
-    font_name = fm.FontProperties(fname=font_path_).get_name() 
-    fm._rebuild()
+    # font_path_ = "data/NanumGothic.ttf" 
+    # font_name = fm.FontProperties(fname=font_path_).get_name() 
 
-    mpl.rcParams['axes.unicode_minus'] = False 
-    mpl.rcParams['font.family'] = font_name 
+    # mpl.rcParams['axes.unicode_minus'] = False 
+    # mpl.rcParams['font.family'] = font_name 
 
     # plt.style.use('ggplot') 
   
