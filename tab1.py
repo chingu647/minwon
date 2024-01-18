21b8565a703216019d074f012281ace3867f9898
chingu647 <chingu647@gmail.com>
2024-01-18 23:45:06 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -76,13 +76,14 @@ def run_tab():
 
     plt.style.use('ggplot') 
     # ===================================================================
-
+    sns.set_style('whitegrid')
+    
     data_x = t1_body1_df_gby_kind.index.values
     data_y = t1_body1_df_gby_kind['건수']
 
     fig1, ax1 = plt.subplots(figsize=(10,4)) 
     ax1.axis('on') 
-    sns.barplot(x=data_x, y=data_y, ax=ax1, hue='class')
+    sns.barplot(x=data_x, y=data_y, ax=ax1)
     # ax1.bar(data_x, data_y)
     t1_body5.pyplot(fig1) 
     
