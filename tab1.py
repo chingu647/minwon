031e4e9205e18b206c2e96d2f4d080fa6cf05a00
chingu647 <chingu647@gmail.com>
2024-01-18 15:12:56 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -50,7 +50,7 @@ def run_tab():
     # -----------------------------------------------------------------------  
     t1_body2.markdown("###### 주요 키워드 클라우드") 
     tw_ = Twitter()
-    text = tw_.nouns('한국어 분석을 시작합니다. 재미 있어요~~~')
+    text_data = tw_.nouns('한국어 분석을 시작합니다. 재미 있어요~~~')
     # text_data = '한국, 한국, korea, korea, usa, england, highway, service, highway'
     wc = WordCloud(background_color='white', font_path=r"data/NanumGothic.ttf").generate(text_data) 
 
