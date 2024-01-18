cfd40feae14c1763e9e97be07ea8d51b91728807
chingu647 <chingu647@gmail.com>
2024-01-18 22:20:03 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -80,7 +80,7 @@ def run_tab():
 
     fig1, ax1 = plt.subplots(figsize=(10,4)) 
     ax1.axis('off') 
-    ax1.plot(x=data_x, y=data_y, kind='bar')
+    ax1.plot(data_x, data_y, kind='bar')
     t1_body2.pyplot(fig1) 
     
     # -----------------------------------------------------------------------  
