04bb7d17f0b405eb026adaaec3fb799bf29d38a6
chingu647 <chingu647@gmail.com>
2024-01-19 17:09:08 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -109,7 +109,7 @@ def run_tab():
             startangle=30,
             autopct="%.1f%%",
             shadow=True,
-            textprops={'size':8}) 
+            textprops={'size':9}) 
 
     # bar ------------------------------------
 
