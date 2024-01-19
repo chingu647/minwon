5e1f5cb4c4decfafdcfdaf8c2d52a275ad77cf2d
chingu647 <chingu647@gmail.com>
2024-01-19 13:02:49 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -105,7 +105,7 @@ def run_tab():
     ax1.set_facecolor('yellow')                          # figure 배경색 
 
     # pie ------------------------------------
-    ax1.pie(data_y, figsize=(5,5), startangle=10)    
+    ax1.pie(data_y, figsize=(5,5), labels=data_x)    
 
     # bar ------------------------------------
 
