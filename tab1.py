8e3c199da8b51ecc94315da644231957c3b0e6a5
chingu647 <chingu647@gmail.com>
2024-01-19 10:43:53 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -87,11 +87,13 @@ def run_tab():
     data_y = t1_body1_df_gby_kind['건수']
 
     fig1, ax1 = plt.subplots(figsize=(10,4)) 
-    ax1.tick_params(#labelsize=20,
-                    bottom = True, labelbottom=True,    # tick 수정
-                    top = False, labeltop=False,
-                    left = True, labelleft=True,
-                    right= True, labelright=True)
+    ax1.tick_params(
+        # labelsize=20,
+        bottom = True, labelbottom=True,    # tick 수정
+        # top = False, labeltop=True,
+        # left = True, labelleft=True,
+        # right= True, labelright=True
+        )
     
     ax1.bar(data_x, data_y)                             # plot 표시
     for i in range(len(data_x)):                        # text 표시
