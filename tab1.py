d9c3b8a8ae74d7699f375d7fca13d727f70a0fc5
chingu647 <chingu647@gmail.com>
2024-01-19 10:38:45 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -87,11 +87,11 @@ def run_tab():
     data_y = t1_body1_df_gby_kind['건수']
 
     fig1, ax1 = plt.subplots(figsize=(10,4)) 
-    ax1.axis('on') 
+    ax1.tick_params(labelsize=30)
     ax1.bar(data_x, data_y) 
     for i in range(len(data_x)):
-        height = data_y[i]+0.25 
-        ax1.text(data_x[i], height, data_y[i]) 
+        height = data_y[i]-0.5 
+        ax1.text(data_x[i], height, data_y[i], ha='center', va='bottom') 
     # sns.barplot(x=data_x, y=data_y, ax=ax1, dodge=False)
     ax1.bar(data_x, data_y)
     t1_body5.pyplot(fig1) 
