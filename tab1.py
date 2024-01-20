d40701bfe55d15bb57aa7cd5cd8932b72b8c259a
chingu647 <chingu647@gmail.com>
2024-01-20 20:03:12 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -84,7 +84,7 @@ def run_tab():
                                     ).add_to(map) 
                 
                 folium.Marker( location=[ row['latitude'], row['longitude'] ],  # 값 중심 
-                            popup=row['latitude'], 
+                            popup=row['제목'], 
                             tooltip=row['latitude'], 
                             icon=folium.Icon(color='red', icon='star'), 
                             #   icon=folium.DivIcon(                              # 값 표시방식
