502354492ceb3ee0cb42ae1abcede4f3a42bd434
chingu647 <chingu647@gmail.com>
2024-01-20 21:20:01 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -273,7 +273,7 @@ def run_tab():
 
     t1t1_kind1_df, t1t1_point_df = load_df('광주지사', '서비스유형(대)')   
     t1t1_point_df_temp = t1t1_point_df.copy()  
-    t1t1.table(t1t1_point_df_temp)
+    t1t1.dataframe(t1t1_point_df_temp)
 
     base_position = [35.18668601, 126.87954220] 
     t1t1_map = load_map(base_position) 
