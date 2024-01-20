8c5a807fb85a3feb82060508caffdbf2740d4cec
chingu647 <chingu647@gmail.com>
2024-01-20 20:59:30 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -174,7 +174,7 @@ def run_tab():
     t1b5_ax.text(t1b5_cx, t1b5_cy, t1b5_val, 
                 ha='center', va='center', 
                 color='red',
-                fontsize=25)                           # bar text 폰크 
+                fontsize=23)                           # bar text 폰크 
 
     t1b5.pyplot(t1b5_fig) 
 
