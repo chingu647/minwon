503161557736f8465530d1b7390f6de66d7f2548
chingu647 <chingu647@gmail.com>
2024-01-20 20:54:40 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -168,8 +168,8 @@ def run_tab():
             # shadow=True,
             wedgeprops=wedgeprops, 
             textprops={'size':9}) 
-    t1b5_cx = 0.15
-    t1b5_cy = 0.35
+    t1b5_cx = 0.05
+    t1b5_cy = 0.05
     t1b5_val= str(t1b5_kind1_df['건수'].sum()) + '건'
     t1b5_ax.text(t1b5_cx, t1b5_cy, t1b5_val, 
                 ha='center', va='bottom', 
