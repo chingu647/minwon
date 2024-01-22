1cba67b85be26c0766ac4cb85e4918dda1baff1d
chingu647 <chingu647@gmail.com>
2024-01-22 20:10:36 +0900

    all...

--- tab0.py
+++ tab0.py
@@ -32,7 +32,7 @@ def run_tab():
                 <style> 
                     table{background-color:#f0f0f0;} 
                     # div{border:1px solid #00ff00;}
-                    img {max-width: 600px; max-height: 800px;}    # 이미지 파일 최대크기 제한 
+                    img {max-width: 600px; max-height: 600px;}    # 이미지 파일 최대크기 제한 
                 
                 </style> """, 
                 unsafe_allow_html=True
