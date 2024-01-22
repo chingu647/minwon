e98cd4ea1a53e54c6c3ded52a690d7e50d062e8e
chingu647 <chingu647@gmail.com>
2024-01-22 21:27:08 +0900

    all...

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ import mf
 
 
 import tab0 
-# import tab1 
-# import tab2
-# import tab3 
-# import tab4 
-# import tab5 
-# import tab6 
-# import tab7 
+import tab1 
+import tab2
+import tab3 
+import tab4 
+import tab5 
+import tab6 
+import tab7 
 
 st.set_page_config(layout="wide")  #### 1번만 실행해야 함 !!! 
 
@@ -105,33 +105,33 @@ with tabs[0]:
     tab0.run_tab()
     st.balloons()
  
-# with tabs[1]:
-#     tab1.run_tab()
-#     st.balloons()
+with tabs[1]:
+    tab1.run_tab()
+    st.balloons()
 
-# with tabs[2]:
-#     tab2.run_tab()
-#     st.balloons()
+with tabs[2]:
+    tab2.run_tab()
+    st.balloons()
 
-# with tabs[3]: 
-#     tab3.run_tab()
-#     st.balloons()
+with tabs[3]: 
+    tab3.run_tab()
+    st.balloons()
 
-# with tabs[4]:
-#     tab4.run_tab() 
-#     st.balloons()
+with tabs[4]:
+    tab4.run_tab() 
+    st.balloons()
 
-# with tabs[5]:
-#     tab5.run_tab()
-#     st.balloons()
+with tabs[5]:
+    tab5.run_tab()
+    st.balloons()
 
-# with tabs[6]:
-#     tab6.run_tab()
-#     st.balloons()
+with tabs[6]:
+    tab6.run_tab()
+    st.balloons()
 
-# with tabs[7]:
-#     tab7.run_tab()
-#     st.balloons()
+with tabs[7]:
+    tab7.run_tab()
+    st.balloons()
 
 ################################################################################# input -> layout
 # 셀렉트 박스 -----------------------------------------------------------------
