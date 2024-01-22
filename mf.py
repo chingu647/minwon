6e156beba04a8118dfb81ba2cd8f5dd62c46e962
chingu647 <chingu647@gmail.com>
2024-01-22 19:58:14 +0900

    all...

---
+++ mf.py
@@ -0,0 +1,238 @@
+import streamlit as st 
+import plotly.express as px
+import pandas as pd
+import numpy as np 
+
+import matplotlib as mpl 
+import matplotlib.pyplot as plt 
+import matplotlib.font_manager as fm 
+import seaborn as sns
+
+import geopandas as gpd 
+import folium 
+from streamlit_folium import folium_static 
+
+import nltk 
+from konlpy.tag import Kkma, Hannanum, Twitter, Okt
+from wordcloud import WordCloud, STOPWORDS 
+
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ (3-1) ST CACHE 사용
+
+##################################################################################### load_df 
+# arg1 : global organ_ t?? ---------- 탭 페이지에서 입력
+# arg2 : global kind1_ t?? ---------- 탭 페이지에서 입력
+@st.cache_resource 
+def load_df(organ, kind1):
+    df = pd.read_csv("data/민원처리현황.csv")
+    df = df.query( f"organ=='{organ}'" )
+    kind1_df = df.groupby(by=f'{kind1}').count() #.sort_values(by=f'{kind1}', ascending=False)
+    kind1_df = kind1_df.iloc[:5,:1]
+    kind1_df.columns = ['건수']
+    kind1_df['비율(%)'] = ( kind1_df['건수']/(kind1_df['건수'].sum())*100).astype(int)
+    kind1_df = kind1_df.sort_values(by='건수', ascending=False) 
+
+    # map data
+    # point_df = df[ (df['latitude'].str.strip() != '') and (df['longitude'].str.strip() != '') ] 
+    point_df = df[ ~( (df['latitude'].isna()) | (df['longitude'].isna()) ) ] 
+
+    return kind1_df, point_df  
+    # kind1_df --------- 
+    # point_df --------- 
+
+
+
+##################################################################################### load wc 
+# arg1 : text_raw 
+# arg2 : target_fig --------- 탭 페이지에서 입력
+@st.cache_resource 
+def load_wc(text_raw, target_layout):
+    t = Okt()
+    text_nouns = t.nouns(text_raw) 
+    stopwords =['시어']
+    text_nouns = [n for n in text_nouns if n not in stopwords]
+    text_str = ' '.join(text_nouns)
+    wc = WordCloud(background_color='#ECF8E0', font_path=r"data/NanumGothic.ttf", max_words=50).generate(text_str) 
+    
+    fig, ax = plt.subplots(figsize=(10,4)) 
+    ax.axis('off')
+    ax.imshow(wc) 
+    target_layout.pyplot(fig) 
+
+
+
+##################################################################################### load map
+# arg1 : base_position_ t?? ---------- 탭 페이지에서 입력
+# arg2 : t?? _point_df ---------- 위의 load_df에서 받아 옴
+@st.cache_resource 
+def load_map(base_position, point_df): 
+    map = folium.Map( location=base_position, zoom_start=9 ) #, tiles='Stamentoner') 
+    gpf_line = gpd.read_file("data/ex_line_KWANGJU.shp") 
+    folium.GeoJson(gpf_line, 
+                    style_function=lambda feature: {
+                        'fillColor': 'blue' , #feature['properties']['color'],
+                        'color': '#F5F6CE',
+                        'weight': 3,
+                        'dashArray': '5, 5',
+                        'fillOpacity': 0.3, 
+                    }
+                ).add_to(map) 
+
+    for index, row in point_df.iterrows(): 
+        if not pd.isna(row['latitude']) and not pd.isna(row['longitude']):
+            folium.CircleMarker( location=[ row['latitude'], row['longitude'] ],  # 원 중심
+                                radius=1,            # 원 반지름
+                                color='blue',        # 원 테두리 색상
+                                fill=True,           # 원 채움
+                                fill_opacity=0.5,     # 원 채움 투명도
+                                ).add_to(map) 
+            
+            folium.Marker( location=[ row['latitude'], row['longitude'] ],  # 값 중심 
+                        popup=f"{row['서비스유형(소)']} ( {row['고객유형']} ) ", 
+                        tooltip=row['latitude'], 
+                        icon=folium.Icon(color='red', icon='star'), 
+                        #   icon=folium.DivIcon(                              # 값 표시방식
+                        #       html=f"<div>{row['노선번호']} {row['latitude']} {row['longitude']}</div>"),
+                        ).add_to(map) 
+
+    folium_map = map._repr_html_() 
+    st.components.v1.html(folium_map, height=900) #, width=800, height=600)
+    # folium_static(t1_map) #, width=600, height=400)
+    # t1_tail1.map(data=t1_gpf, latitude='latitude', longitude='longitude')  
+
+
+
+##################################################################################### create pie
+# arg1 : organ_ t?? --------- 탭 페이지에서 입력 
+# arg2 : kind1_ t?? --------- 탭 페이지에서 입력 
+# arg3 : target_layout --------- 탭 페이지에서 입력
+def create_pie(organ, kind1, target_layout): 
+    # data  
+    kind1_df, _ = load_df(organ, kind1)  #   <==================================================
+
+    data_x = kind1_df.index.values
+    data_y = kind1_df['건수'] 
+
+    # preprocessing
+    fig, ax = plt.subplots(figsize=(10,4)) 
+    ax.tick_params(
+        # axis=x or axis=y,
+        # labelsize=20,
+        direction='inout',
+        color = 'red',
+        colors = 'blue',
+        # rotation=20, 
+        bottom = True, labelbottom=True,        # tick 수정
+        top = False, labeltop=False,
+        left = True, labelleft=True,
+        right= False, labelright=False
+        )
+    ax.set_facecolor('white')                  # figure 배경색 
+
+    # paint 
+    explode = [0.05 for i in data_x]
+    wedgeprops={'width': 0.5, 'edgecolor': 'w', 'linewidth': 3}
+    ax.pie(data_y, labels=data_x, 
+           startangle=260,
+           counterclock=False, 
+           autopct="%.1f%%", 
+           # explode=explode,
+           # shadow=True,
+           wedgeprops=wedgeprops, 
+           textprops={'size':9}) 
+    data_cx = 0
+    data_cy = 0
+    data_val= '총' + str(kind1_df['건수'].sum()) + '건'
+    ax.text(data_cx, data_cy, data_val, 
+            ha='center', va='center', 
+            color='red',
+            fontsize=23)                           # bar text 폰크 
+    
+    target_layout.pyplot(fig) 
+
+
+
+##################################################################################### 세로 막대 create vbar 
+# arg1 : organ_ t?? --------- 탭 페이지에서 입력 
+# arg2 : kind1_ t?? --------- 탭 페이지에서 입력 
+# arg3 : target_layout --------- 탭 페이지에서 입력
+def create_vbar(organ, kind1, target_layout): 
+    # data  
+    kind1_df, _ = load_df(organ, kind1)  #   <==================================================
+
+    data_x = kind1_df.index.values
+    data_y = kind1_df['건수'] 
+
+    # preprocessing 
+    fig, ax = plt.subplots(figsize=(10,4)) 
+    ax.tick_params(
+        # axis=x or axis=y,
+        labelsize=20,
+        direction='inout',
+        color = 'red',
+        colors = 'blue',
+        # rotation=20, 
+        bottom = True, labelbottom=True,        # tick 수정
+        top = False, labeltop=False,
+        left = False, labelleft=False,
+        right= False, labelright=False,  
+        )
+    ax.set_facecolor('white')                  # figure 배경색 
+
+    # paint 
+    ax.bar(data_x, data_y, color='#E0ECF8')            # bar plot 표시
+    for i in range(len(data_x)):                        # bar text 표시
+        height = data_y[i]+0.5 
+        height_str = str(data_y[i])+'건'
+        ax.text(data_x[i], height, height_str, 
+                ha='center', va='bottom', 
+                color='green',
+                fontsize=20)                           # bar text 폰크 
+    
+    target_layout.pyplot(fig) 
+
+
+
+##################################################################################### 가로 막대 create sns hbar 
+# arg1 : organ_ t?? --------- 탭 페이지에서 입력 
+# arg2 : kind1_ t?? --------- 탭 페이지에서 입력 
+# arg3 : target_layout --------- 탭 페이지에서 입력
+def create_sns_hbar(organ, kind1, target_layout): 
+    # data  
+    kind1_df, _ = load_df(organ, kind1)  #   <==================================================
+
+    data_x = kind1_df.index.values
+    data_y = kind1_df['건수'] 
+
+    # preprocessing ---------------------------
+    fig,  ax = plt.subplots(figsize=(10,6)) 
+    ax.tick_params(
+        # axis=x or axis=y,
+        labelsize=20,
+        direction='inout',
+        color = 'red',
+        colors = 'blue',
+        # rotation=20, 
+        bottom = False, labelbottom=False,        # tick 수정
+        top = False, labeltop=False,
+        left = True, labelleft=True,
+        right= False, labelright=False
+        )
+    ax.set_facecolor('white')                  # figure 배경색 
+
+    # paint 
+    sns.barplot(x=data_y, y=data_x, 
+                hue=data_x, 
+                dodge=False,
+                ax=ax) 
+
+    for i in range(len(data_x)):               # bar text 표시
+        width = data_y[i]+1.5 
+        width_str = str(data_y[i])+'건'
+        ax.text(width, i, width_str,
+                # ha='center', va='bottom', 
+                color='green',
+                fontsize=20)                   # bar text 폰크
+
+    target_layout.pyplot(fig)  
+
--- tab0.py
+++ tab0.py
@@ -16,76 +16,16 @@ import nltk
 from konlpy.tag import Kkma, Hannanum, Twitter, Okt
 from wordcloud import WordCloud, STOPWORDS 
 
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ (3-1) ST CACHE 사용
+import mf 
 
 def run_tab(): 
-    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ (3-2) ST CACHE 사용
-    @st.cache_resource 
-    def load_df(organ, kind1):
-        df = pd.read_csv("data/민원처리현황.csv")
-        df = df.query( f"organ=='{organ}'" )
-        kind1_df = df.groupby(by=f'{kind1}').count() #.sort_values(by=f'{kind1}', ascending=False)
-        kind1_df = kind1_df.iloc[:5,:1]
-        kind1_df.columns = ['건수']
-        kind1_df['비율(%)'] = ( kind1_df['건수']/(kind1_df['건수'].sum())*100).astype(int)
-        kind1_df = kind1_df.sort_values(by='건수', ascending=False) 
-
-        # map data
-        # point_df = df[ (df['latitude'].str.strip() != '') and (df['longitude'].str.strip() != '') ] 
-        point_df = df[ ~( (df['latitude'].isna()) | (df['longitude'].isna()) ) ] 
-
-        return kind1_df, point_df  
-
-
-    @st.cache_resource 
-    def load_wc(text_raw):
-        t = Okt()
-        text_nouns = t.nouns(text_raw) 
-        stopwords =['시어']
-        text_nouns = [n for n in text_nouns if n not in stopwords]
-        text_str = ' '.join(text_nouns)
-        wc = WordCloud(background_color='#ECF8E0', font_path=r"data/NanumGothic.ttf", max_words=50).generate(text_str) 
-        
-        return wc 
-
-    @st.cache_resource 
-    def load_map(base_position): 
-        map = folium.Map( location=base_position, zoom_start=9 ) #, tiles='Stamentoner') 
-        gpf_line = gpd.read_file("data/ex_line_KWANGJU.shp") 
-        folium.GeoJson(gpf_line, 
-                        style_function=lambda feature: {
-                            'fillColor': 'blue' , #feature['properties']['color'],
-                            'color': '#F5F6CE',
-                            'weight': 3,
-                            'dashArray': '5, 5',
-                            'fillOpacity': 0.3, 
-                        }
-                    ).add_to(map) 
-        return map
-
-    # @st.cache_resource
-    def create_map(map, point_df): 
-        for index, row in point_df.iterrows(): 
-            if not pd.isna(row['latitude']) and not pd.isna(row['longitude']):
-                folium.CircleMarker( location=[ row['latitude'], row['longitude'] ],  # 원 중심
-                                    radius=1,            # 원 반지름
-                                    color='blue',        # 원 테두리 색상
-                                    fill=True,           # 원 채움
-                                    fill_opacity=0.5,     # 원 채움 투명도
-                                    ).add_to(map) 
-                
-                folium.Marker( location=[ row['latitude'], row['longitude'] ],  # 값 중심 
-                            popup=f"{row['서비스유형(소)']} ( {row['고객유형']} ) ", 
-                            tooltip=row['latitude'], 
-                            icon=folium.Icon(color='red', icon='star'), 
-                            #   icon=folium.DivIcon(                              # 값 표시방식
-                            #       html=f"<div>{row['노선번호']} {row['latitude']} {row['longitude']}</div>"),
-                            ).add_to(map) 
-
-        folium_map = map._repr_html_() 
-        st.components.v1.html(folium_map, height=900) #, width=800, height=600)
-        # folium_static(t1_map) #, width=600, height=400)
-        # t1_tail1.map(data=t1_gpf, latitude='latitude', longitude='longitude')  
-
+    # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ global 변수 설정
+    global t0_map  # ----------------------------------------------------------------------- 
+    global organ_t0
+    global kind1_t0 
+    organ_t0 = "광주지사" 
+    kind1_t0 = '서비스유형(대)'
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ (3-3) css 설정
     st.markdown(""" 
@@ -98,6 +38,8 @@ def run_tab():
                 unsafe_allow_html=True
                 ) 
     
+
+
     # ###################################################################### layout 
     t0h0, t0h1, t0h2 = st.columns( [0.001, 0.998, 0.001] ) 
     
@@ -107,11 +49,15 @@ def run_tab():
 
     t0t0, t0t1, t0t2 = st.columns( [0.001, 0.998, 0.001] ) 
 
+
+
     # ###################################################################### head 1  
     t0h1.markdown("##### 공지사항")
     t0h1.markdown(r"""
 	1. 오늘의 이슈. 
     """) 
+
+
 
     # ###################################################################### body 1  
     t0b1.markdown("##### 2024년 이달의 이슈")
@@ -119,92 +65,27 @@ def run_tab():
 	1. 오늘의 이슈.
     """) 
 
+
+
     # ###################################################################### body 2 
 
     t0b2.markdown("##### 주요 키워드 클라우드") 
 
     text_raw = '한국어 분석을 시작합니다... 재미있어요!!!~~~한국어 분석 고속도로 포장 포장 광주 광주지사 시어요!!!~~~한국어 합니다... 재미있어요!!!~~~'
-    t0b2_wc = load_wc(text_raw)
-  
-    t0b2_fig, t0b2_ax = plt.subplots(figsize=(10,4)) 
-    t0b2_ax.axis('off')
-    t0b2_ax.imshow(t0b2_wc)
-    t0b2.pyplot(t0b2_fig)  
+    t0b2_wc = mf.load_wc(text_raw, t0b2)
+
+
 
     ###################################################################### body 5 
     t0b5.markdown("##### 지사별 민원") 
 
     # -------------------------------------------------------- 세로 bar 그래프 
-    # data  
-    t0b5_kind1_df, _ = load_df('광주지사', '서비스유형(대)') 
+    mf.create_vbar(organ_t0, kind1_t0)
 
-    t0b5_x = t0b5_kind1_df.index.values
-    t0b5_y = t0b5_kind1_df['건수'] 
 
-    # preprocessing 
-    t0b5_fig, t0b5_ax = plt.subplots(figsize=(10,4)) 
-    t0b5_ax.tick_params(
-        # axis=x or axis=y,
-        labelsize=20,
-        direction='inout',
-        color = 'red',
-        colors = 'blue',
-        # rotation=20, 
-        bottom = True, labelbottom=True,        # tick 수정
-        top = False, labeltop=False,
-        left = False, labelleft=False,
-        right= False, labelright=False
-        )
-    t0b5_ax.set_facecolor('white')                  # figure 배경색 
-
-    # paint 
-    t0b5_ax.bar(t0b5_x, t0b5_y, color='#E0ECF8')            # bar plot 표시
-    for i in range(len(t0b5_x)):                        # bar text 표시
-        height = t0b5_y[i]+0.5 
-        height_str = str(t0b5_y[i])+'건'
-        t0b5_ax.text(t0b5_x[i], height, height_str, 
-                 ha='center', va='bottom', 
-                 color='green',
-                 fontsize=20)                           # bar text 폰크 
-
-    t0b5.pyplot(t0b5_fig) 
 
     ###################################################################### body 6
     t0b6.markdown("##### 유형별 민원") 
     
     # -------------------------------------------------------- 가로 sns bar 그래프 
-    # data  
-    t0b6_kind1_df, _ = load_df('광주지사', '서비스유형(대)') 
-    t0b6_x = t0b6_kind1_df.index.values
-    t0b6_y = t0b6_kind1_df['건수'] 
-
-    # preprocessing ---------------------------
-    t0b6_fig,  t0b6_ax = plt.subplots(figsize=(10,6)) 
-    t0b6_ax.tick_params(
-        # axis=x or axis=y,
-        labelsize=20,
-        direction='inout',
-        color = 'red',
-        colors = 'blue',
-        # rotation=20, 
-        bottom = False, labelbottom=False,        # tick 수정
-        top = False, labeltop=False,
-        left = True, labelleft=True,
-        right= False, labelright=False
-        )
-    t0b6_ax.set_facecolor('white')                  # figure 배경색 
-
-    # paint 
-    sns.barplot(x=t0b6_y, y=t0b6_x, 
-                hue=t0b6_x, 
-                dodge=False,
-                ax=t0b6_ax) 
-    for i in range(len(t0b6_x)):               # bar text 표시
-        width = t0b6_y[i]+1.5 
-        width_str = str(t0b6_y[i])+'건'
-        t0b6_ax.text(width, i, width_str, 
-                #  ha='center', va='bottom', 
-                 color='green',
-                 fontsize=20)                   # bar text 폰크
-
-    t0b6.pyplot(t0b6_fig)  
+    mf.create_sns_hbar(organ_t0, kind1_t0, t0b6)
