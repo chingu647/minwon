f72826fe730590b94541fee8e29ec599a685a9a7
chingu647 <chingu647@gmail.com>
2024-01-22 21:48:41 +0900

    all...

--- mf.py
+++ mf.py
@@ -56,7 +56,7 @@ def load_wc(organ, kind1): # target_layout 에러 발생
     text_nouns = t.nouns(wc_data) 
     stopwords =['시어','및','조치','예정','민원','처리','해당','통해','대한','방향','후','검토','확인','완료','내','노력','등','위해','지사',
                 '대하','도록','말씀','수','음','귀하','주신','답변','향','중','향','사항','아래','다음','문의사항','내용','요청','요지','안내',
-                '부분','미완료','대해','관내','박준혁 대리',]
+                '부분','미완료','대해','관내','박준혁','대리','박준혁 대리',]
     text_nouns = [n for n in text_nouns if n not in stopwords]
     text_str = ' '.join(text_nouns)
     wc = WordCloud(background_color='#ECF8E0', font_path=r"data/NanumGothic.ttf", max_words=20).generate(text_str) 
