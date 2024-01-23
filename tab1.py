f8e2049ad7982e1d0510cd2ff7dbc77637ff493a
chingu647 <chingu647@gmail.com>
2024-01-23 21:07:42 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -61,8 +61,7 @@ def run_tab():
 
     ###################################################################### body 1  
     t1b1.divider() 
-    t1b1.markdown("##### 2024년 이슈") 
-    t1b1.caption(f'Here is :blue[{organ_t1}] :sunglasses:')    
+    t1b1.markdown(f"##### 2024년 이슈 --- Here is :blue[{organ_t1}] :sunglasses:") 
 
     t1b1_kind1_df, _, _ = mf.load_df(organ_t1, kind1_t1) 
 
