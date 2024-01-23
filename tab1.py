3ce42167be748964e2b1e361b118df71ad4f66d1
chingu647 <chingu647@gmail.com>
2024-01-23 21:01:19 +0900

    all...

--- tab1.py
+++ tab1.py
@@ -82,7 +82,7 @@ def run_tab():
     t1b2_fig = mf.load_wc(organ_t1, kind1_t1)
     t1b2.pyplot(t1b2_fig, use_container_width=True)    
 
-    t1b1.markdown(r"""
+    t1b2.markdown(r"""
 	1. 광주지사 민원은 증가추세에 있습니다.
     """)
 
@@ -95,7 +95,7 @@ def run_tab():
     t1b5_pie = mf.create_pie(organ_t1, kind1_t1)
     t1b5.pyplot(t1b5_pie, use_container_width=True)    
 
-    t1b1.markdown(r"""
+    t1b5.markdown(r"""
 	1. 광주지사 민원은 증가추세에 있습니다.
     """)
 
