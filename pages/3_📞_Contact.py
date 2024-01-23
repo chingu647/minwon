7bd6a0d768a12d77bf97620112c9601dba94725e
chingu647 <chingu647@gmail.com>
2024-01-24 00:22:22 +0900

    all...

--- pages/3_📞_Contact.py
+++ pages/3_📞_Contact.py
@@ -10,6 +10,28 @@ import matplotlib.font_manager as fm
 
 import seaborn as sns 
 
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ global 변수 설정
+# global map_t1  # ----------------------------------------------------------------------- 
+# global organ_t1
+# global kind1_t1 
+# global base_position_t1
+
+# organ_t1 = "광주지사" 
+# kind1_t1 = '서비스유형(대)'
+# base_position_t1 = [35.18668601, 126.87954220] 
+
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ (3-3) css 설정
+st.markdown(""" 
+            <style> 
+                table{background-color:#f0f0f0;} 
+                img {max-width: 1000px; max-height: 600px; }    # 이미지 파일 최대크기 제한 
+            
+            </style> """, 
+            unsafe_allow_html=True
+            ) 
+
+
 st.title('Contact')
 
 # ##################################################################### layout 
