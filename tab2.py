c1c9e9f49947b03c780d145d27c35f7de371c5ab
chingu647 <chingu647@gmail.com>
2024-01-23 10:51:22 +0900

    all...

--- tab2.py
+++ tab2.py
@@ -103,7 +103,7 @@ def run_tab():
     t2b5.markdown(""" 
         <style> 
             table{background-color:#f0f0f0;} 
-            img {max-width:200px;max-height:200px; object-fit:cover;}  
+            img {max-width:100px;max-height:100px; object-fit:cover;}  
         </style> """, 
         unsafe_allow_html=True
         ) 
