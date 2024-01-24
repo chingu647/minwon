aaa50ae5b67acaf78985dc817ab1f9220ef52d47
chingu647 <chingu647@gmail.com>
2024-01-24 20:47:02 +0900

    all...

--- mf.py
+++ mf.py
@@ -242,7 +242,7 @@ def load_map_kind1(organ, kind1, base_position):
 
     GroupedLayerControl(groups={  'groups1': [fg_k0, fg_k1, fg_k2, fg_k3, fg_k4]  }, 
                         exclusive_groups=False, 
-                        collapsed=False, 
+                        collapsed=True, 
                         ).add_to(map)
 
 
