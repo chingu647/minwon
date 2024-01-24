4390a4033e181c68aac101a43a1005aecd261fc9
chingu647 <chingu647@gmail.com>
2024-01-24 20:50:46 +0900

    all...

--- mf.py
+++ mf.py
@@ -240,7 +240,7 @@ def load_map_kind1(organ, kind1, base_position):
     # 
     # folium.LayerControl(collapsed=False).add_to(map)
 
-    GroupedLayerControl(groups={  'groups1': [fg_k0, fg_k1, fg_k2, fg_k3, fg_k4]  }, 
+    GroupedLayerControl(groups={  f'{kind1}': [fg_k0, fg_k1, fg_k2, fg_k3, fg_k4]  }, 
                         exclusive_groups=False, 
                         collapsed=True, 
                         ).add_to(map)
