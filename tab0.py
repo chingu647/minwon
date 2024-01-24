c44f8546f0eb1e856d5e797d2d6bafb79a21e013
chingu647 <chingu647@gmail.com>
2024-01-24 21:42:45 +0900

    all...

--- tab0.py
+++ tab0.py
@@ -65,7 +65,7 @@ def run_tab():
     t0b1_kind1_df, _, _ = mf.load_df(organ_t0, kind1_t0) 
 
     t0b1.markdown(f"""
-	<center>최근 이슈</font>는 <font color='red'>{t0b1_kind1_df.index[0]} > {t0b1_kind1_df.index[1]} > {t0b1_kind1_df.index[2]}</font> 순 입니다.</center>
+	<center>최근 이슈</font>는 <font color='red'><b>{t0b1_kind1_df.index[0]}</b> > {t0b1_kind1_df.index[1]} > {t0b1_kind1_df.index[2]}</font> 순 입니다.</center>
     """, unsafe_allow_html=True) 
 
     t0b1.table(t0b1_kind1_df.style.background_gradient(cmap='Blues')) 
