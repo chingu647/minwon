e9fce84f01e6d06a27ebfdde3a301c788a57f283
chingu647 <chingu647@gmail.com>
2024-01-24 17:05:15 +0900

    all...

--- tab0.py
+++ tab0.py
@@ -129,10 +129,23 @@ def run_tab():
 
 
 
-    df0,df1,df2,df3,df4 = mf.load_map_kind1(organ_t0, kind1_t0, base_position_t0) 
-    t0t1.table(df0) 
-    t0t1.table(df1) 
-    t0t1.table(df2) 
-    t0t1.table(df3) 
-    t0t1.table(df4)
+    # df0,df1,df2,df3,df4 = mf.load_map_kind1(organ_t0, kind1_t0, base_position_t0) 
+    # t0t1.table(df0) 
+    # t0t1.table(df1) 
+    # t0t1.table(df2) 
+    # t0t1.table(df3) 
+    # t0t1.table(df4) 
+
+
+
+
+    # 예제 데이터프레임
+    df = pd.DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
+
+    # 잘못된 query 사용 예시
+    condition = (df['A'] == 1) 
+    t0t1.write( df['A'] )
+    t0t1.write( (df['A'] == 1) )
+    t0t1.write(condition)
+    # result = df.query(condition)
 
