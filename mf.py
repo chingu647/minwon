74c844931f464309e5a4cdc48963cd12f256daaf
chingu647 <chingu647@gmail.com>
2024-01-26 08:59:13 +0900

    all...

--- mf.py
+++ mf.py
@@ -49,12 +49,14 @@ def load_df(organ, kind1):
     # 위경도 없는 자료는 제외 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     point_df = df[ ~( (df[f'{LATITUDE}'].isna()) | (df[f'{LONGITUDE}'].isna()) ) ] 
 
+    st.write(point_df)
+    
     kind1_df = point_df.groupby(by=f'{kind1}').count() #.sort_values(by=f'{kind1}', ascending=False)
-    kind1_df = kind1_df.iloc
-    kind1_df.columns = ['건수']
-    kind1_df['건수'] = kind1_df['건수'].astype(int)
-    kind1_df['비율(%)'] = ( kind1_df['건수']/(kind1_df['건수'].sum())*100).astype(int)
-    kind1_df = kind1_df.sort_values(by='건수', ascending=False) 
+    # kind1_df = kind1_df.iloc
+    # kind1_df.columns = ['건수']
+    # kind1_df['건수'] = kind1_df['건수'].astype(int)
+    # kind1_df['비율(%)'] = ( kind1_df['건수']/(kind1_df['건수'].sum())*100).astype(int)
+    # kind1_df = kind1_df.sort_values(by='건수', ascending=False) 
 
     # map data
     # point_df = df[ (df['latitude'].str.strip() != '') and (df['longitude'].str.strip() != '') ] 
