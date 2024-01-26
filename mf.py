84ad69bd7c978306ebf13c0c9006a428cc44f7ca
chingu647 <chingu647@gmail.com>
2024-01-26 23:51:11 +0900

    all...

--- mf.py
+++ mf.py
@@ -408,7 +408,7 @@ def create_go_vbar(organ, kind1):
 
     fig = go.Figure()
     fig.add_trace(
-        go.Bar(x=month_df.DATE, y=month_df.NUMBER, name='bA')
+        go.Bar(x=month_df.DATE, y=month_df.NUMBER, name='bA'), 
         go.Scatter(x=month_df.DATE, y=month_df.NUMBER, mode='markers', name='sA'), 
                   
     )
