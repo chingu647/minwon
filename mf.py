689e4e34f9fd3ff749b55077256448707f485258
chingu647 <chingu647@gmail.com>
2024-01-26 23:28:28 +0900

    all...

--- mf.py
+++ mf.py
@@ -417,7 +417,7 @@ def create_go_vbar(organ, kind1):
           title=dict(
               text='<b>관련 불량 위치 수</b><br><sup>Check All Error Pin Point by Portion</sup>',        # <br> 태크와 <sup>태그 사용해서 서브 타이틀을 작성할 수 있음 
             #   x=0.87,
-            #   y=0.5,
+            #   y=1.0,
               font=dict(
                   family="Arial",
                   size=25,
