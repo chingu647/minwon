08252bd365c31655a4d926fb342682273e4b1e1a
chingu647 <chingu647@gmail.com>
2024-01-26 22:05:43 +0900

    all...

--- mf.py
+++ mf.py
@@ -403,9 +403,9 @@ def create_plotly_vbar(organ, kind1):
     # data  
     month_df, point_df, kind1_df, wc_data = load_df(organ, kind1)  #   <==================================================
 
-    fig = px.bar(month_df, x='DATE', y=['NUMBER'], color='DATE', 
+    fig = px.bar(month_df, x='DATE', y='NUMBER', color='DATE', 
                  title="민원 건 수 현황",
-                 labels={'NUMBER':'민원 건 수', "DATE":"월별", },
+                 labels={"DATE":"월별", 'NUMBER':'민원 건 수', },
                 #  hover_name='DATE',
                 #  hover_data=['DATE','NUMBER'],  
                 #             #   {'DATE':"|%B, %Y",
