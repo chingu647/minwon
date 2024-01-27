b39d6c64c889b9ae94ca4dff48ed9c38e9f4fcb0
chingu647 <chingu647@gmail.com>
2024-01-27 10:43:38 +0900

    all...

--- mf.py
+++ mf.py
@@ -417,7 +417,7 @@ def create_go_vbar(organ, kind1):
 
     # # Add traces to the subplot grid 
     fig.add_trace(go.Scatter(x=t, y=y1, 
-                             mode="markers", 
+                            #  mode="markers", 
                              fill='tozeroy',
                              marker=dict(size=t*5,
                                          color=t, # 'darkblue', 
