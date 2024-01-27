57cde9fb174d08da96306baeb906da24d589c863
chingu647 <chingu647@gmail.com>
2024-01-27 12:31:01 +0900

    all...

--- mf.py
+++ mf.py
@@ -516,9 +516,8 @@ def create_go_candlestick(organ, kind1):
 def create_go_bar(organ, kind1): 
     # data  
     # month_df, point_df, kind1_df, wc_data = load_df(organ, kind1)  #   <==================================================
-    t = np.random.randint(0, 5, size=5)
-    y1 = np.random.randint(0, 5, size=5)
-    y2 = np.random.randint(0, 5, size=5)
+    t = np.linspace(0, 10, 5)
+    y1 = np.random.randn(5).cumsum()
 
     # @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ Create subplot grid
     fig = make_subplots(rows=1, cols=1,   specs= [  [  {"secondary_y": True}  ]  ]   )
