4605f0abe20876c7f386df38f429149a91b741ca
chingu647 <chingu647@gmail.com>
2024-01-27 12:41:49 +0900

    all...

--- mf.py
+++ mf.py
@@ -555,7 +555,7 @@ def create_go_bar(organ, kind1):
                         #  mode="lines+markers", fill='tonexty', 
                         #  line=dict(width=0.5, 
                         #            color='indigo'),
-                         marker_color = 'mediumvioletred',  # px.colors.qualitative.Dark24,
+                         marker_color = 'blue',  # px.colors.qualitative.Dark24,
                          name="Bar C",
                          text=y3, textposition="inside",   # ['inside', 'outside', 'auto', 'none']
                          hoverinfo="x+y", 
