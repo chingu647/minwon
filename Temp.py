972b9b93cd2318598335d4cc3003c0ced2c5aabd
chingu647 <chingu647@gmail.com>
2024-01-31 08:57:37 +0900

    add ...

--- Temp.py
+++ Temp.py
@@ -122,7 +122,8 @@ def run_tab():
     with tabs[1]: 
     #     # df1_0.columns = ['민원 유형', '발생 건수', '백분율 (%)']         
     #     # cont9.dataframe(df9_1) 
-        tabs[1].dataframe(df9_0, use_container_width=True) 
+        tabs[1].dataframe(df9_1
+                          , use_container_width=True) 
         # tabs[1].dataframe(df9_0) #.style.background_gradient(cmap='Blues'), use_container_width=True) 
 
 
