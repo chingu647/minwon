59c316821c0504946350aae596bab8a9646037fb
chingu647 <chingu647@gmail.com>
2024-01-31 09:38:59 +0900

    all...

--- mf.py
+++ mf.py
@@ -135,7 +135,8 @@ def load_map(organ, kind1, base_position):
                                 ).add_to(map) 
                       
             folium.Marker( location=[ row[f'{LATITUDE}'], row[f'{LONGITUDE}'] ],  # 값 중심 
-                        popup=f"{row[f'{KIND2}']} ( {row[f'{KEYWORD}']} ) ", 
+                        popup=f"{row.index} ( {row[f'{KEYWORD}']} ) ", 
+                        # popup=f"{row[f'{KIND2}']} ( {row[f'{KEYWORD}']} ) ", 
                         tooltip=row[f'{LATITUDE}'], 
                         icon=folium.Icon(color='red', icon='star'), 
                         #   icon=folium.DivIcon(                              # 값 표시방식
@@ -248,8 +249,7 @@ def load_map_kind1(organ, kind1, base_position):
                                 ).add_to(fg_k2) 
                       
             folium.Marker( location=[ row[f'{LATITUDE}'], row[f'{LONGITUDE}'] ],  # 값 중심 
-                        # popup=f"{row[f'{KIND2}']} ( {row[f'{KEYWORD}']} ) ", 
-                        popup=f"{row.index} ( {row[f'{KEYWORD}']} ) ", 
+                        popup=f"{row[f'{KIND2}']} ( {row[f'{KEYWORD}']} ) ", 
                         tooltip=row[f'{LATITUDE}'], 
                         icon=folium.Icon(color='orange', icon='star'), 
                         #   icon=folium.DivIcon(                              # 값 표시방식
