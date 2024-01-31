3e926e0bc84398ae69ed6100f37dbc3024d57dcf
chingu647 <chingu647@gmail.com>
2024-01-31 11:08:48 +0900

    all...

--- mf.py
+++ mf.py
@@ -86,30 +86,32 @@ def load_wc(organ, kind1): # target_layout 에러 발생
                 '문의사항','내용','요청','요지','안내','일부','부분','미완료','관내','박준혁','대리','박준혁 대리','관련','저희','것','함','구간','고객']
     text_nouns = [n for n in text_nouns if n not in stopwords] 
 
-    # # Term Frequency - word_count 옵션 --------------------------------- X
-    # tf_words = []
-    # for i in range(len(text_nouns)):
-    #     tf_words.append(text_nouns[i]) 
-
-    # word_count = {}
-    # for word in tf_words:
-    #     if word in word_count: 
-    #         word_count[word] += 1
-    #     else: 
-    #         word_count[word] = 1
-
-    # Documnent Frequency - word_count 옵션 ------------------------------ ok
-    df_words = []
+    # Term Frequency - word_count 옵션 --------------------------------- X
+    tf_words = []
     for i in range(len(text_nouns)):
-        df_words.append( list(set( text_nouns[i] )) ) 
+        tf_words.append(text_nouns[i]) 
 
-    word_count = {}
-    for words_list in df_words:
-        word = tuple(words_list)
+    tf_words = list( set( tf_words ) ) 
+    
+    word_count = {} 
+    for word in tf_words:
         if word in word_count: 
             word_count[word] += 1
         else: 
             word_count[word] = 1
+
+    # Documnent Frequency - word_count 옵션 ------------------------------ ok
+    # df_words = []
+    # for i in range(len(text_nouns)):
+    #     df_words.append( list(set( text_nouns[i] )) ) 
+
+    # word_count = {}
+    # for words_list in df_words:
+    #     word = tuple(words_list)
+    #     if word in word_count: 
+    #         word_count[word] += 1
+    #     else: 
+    #         word_count[word] = 1
     # ------------------------------------------------------------------
     
     text_str = ' '.join(text_nouns)
