a29a8049d3f1f49816e59ad04f8f4212777869e5
chingu647 <chingu647@gmail.com>
2024-01-31 10:28:57 +0900

    all...

--- Temp.py
+++ Temp.py
@@ -124,7 +124,8 @@ def run_tab():
         df9_1_temp = df9_1.sort_values(by='NUMBER', ascending=False) 
         # tabs[1].write(f"📢 최다 노선은 <strong>{ df3_2_temp.iloc[0][ f'{road}' ] }</strong> 으로, " + 
         #               f"<strong>총 { df3_2_temp.iloc[0][ 'NUMBER' ] } 건 ({ df3_2_temp.iloc[0][ f'NUMBER_pct' ] } %)</strong> 입니다.       , ", unsafe_allow_html=True) 
-        tabs[1].pyplot(fig9_0)  #, use_container_width=True)  
+        # tabs[1].pyplot(fig9_0)  #, use_container_width=True) 
+        fig9_0.to_image()  
 
     with tabs[2]: 
     #     # df1_0.columns = ['민원 유형', '발생 건수', '백분율 (%)']         
