d9b17fb6da7237e04d4988947f173476d00f823a
chingu647 <chingu647@gmail.com>
2024-02-06 14:08:56 +0900

    first deploy ...

--- mf.py
+++ mf.py
@@ -310,9 +310,9 @@ def create_px_bar_month(organ, choice):
     # fig.update_layout(xaxis_visible=False)
     # fig.update_layout(yaxis_visible=False)    
     fig.update_xaxes(showticklabels = True, 
-                     tickformat = '%Y-%m',                # '%d %B (%a)<br>%Y' / '%Y-%b-%d(%a)'
+                    #  tickformat = f'%Y-%m',                # '%d %B (%a)<br>%Y' / '%Y-%b-%d(%a)'
                     #  dtick="M1", 
-                     dtick=30 * 24 * 60 * 60 * 1000,   # 한 달 간격을 밀리초로 계산 
+                    #  dtick=30 * 24 * 60 * 60 * 1000,   # 한 달 간격을 밀리초로 계산 
                     ) 
     return fig, month_df, point_df, choice_df, wc_data
 
